37654f179c2e7f24800c210ba1816543e0811b59
Júlia Zanin Pereira <127359841+juliapzp@users.noreply.github.com>
2024-02-08 09:52:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let hi = document.getElementById('horaInicial');
+let mi = document.getElementById('minInicial');
+let hf = document.getElementById('horaFinal');
+let mf = document.getElementById('minFinal');
+let resultado = document.getElementById('r');
+
+function soma(){
+    let somaHora = Number(hi.value) + Number(hf.value);
+    let somaMinuto = Number(mi.value) + Number(mf.value);
+
+    resultado.innerHTML = somaHora + ":" + somaMinuto;
+}
+
+function sub(){
+    let subHora = Number(hi.value) - Number(hf.value);
+    let subMinuto = Number(mi.value) - Number(mf.value);
+
+    resultado.innerHTML = subHora + ":" + subMinuto;
+
+}
